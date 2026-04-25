5f7ce6863128f7cf9352ad99076646acf6b8fef6
Joe Innes <joe@joeinn.es>
2026-04-25 14:42:38 +0200

    Use PUBLIC_VAPID_KEY from public env in server push

--- src/lib/server/push.ts
+++ src/lib/server/push.ts
@@ -1,7 +1,8 @@
 import webpush from "web-push";
-import { VAPID_PRIVATE_KEY, VAPID_PUBLIC_KEY, VAPID_SUBJECT } from "$env/static/private";
+import { VAPID_PRIVATE_KEY, VAPID_SUBJECT } from "$env/static/private";
+import { PUBLIC_VAPID_KEY } from "$env/static/public";
 
-webpush.setVapidDetails(VAPID_SUBJECT, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY);
+webpush.setVapidDetails(VAPID_SUBJECT, PUBLIC_VAPID_KEY, VAPID_PRIVATE_KEY);
 
 export async function sendPush(subscription: PushSubscriptionJSON) {
   await webpush.sendNotification(
